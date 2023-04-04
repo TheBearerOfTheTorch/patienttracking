39f3a9a765fe8ea63bd6fb86a6221d130d0f07f1
TheBearerOfTheTorch <iithindia@gmail.com>
2023-04-04 09:44:59 +0200

    beta app

--- lib/shared_components/tabs/appointment_tab.dart
+++ lib/shared_components/tabs/appointment_tab.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentTab extends StatelessWidget {
@@ -9,6 +10,31 @@ class AppointmentTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text("Appointment Tab"));
+    final Stream<QuerySnapshot> appointment =
+        FirebaseFirestore.instance.collection('appointment').snapshots();
+    return Scaffold(
+        body: StreamBuilder<QuerySnapshot>(
+            stream: appointment,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasData) {
+                return ListView(
+                  children: snapshot.data!.docs.map((e) {
+                    Map<String, dynamic> data = e.data as Map<String, dynamic>;
+
+                    return ListTile(
+                        leading: Text(data['time']),
+                        title: Column(children: [
+                          Text(data['appointment_type']),
+                          Text(data['doctor_name']),
+                        ]),
+                        trailing: Text(data['date']));
+                  }).toList(),
+                );
+              }
+              return const Center(child: Text('An error occured'));
+            }));
   }
 }
